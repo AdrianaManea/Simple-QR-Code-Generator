55219ce8e2bad00ced16c7662dc53e8fc5168fa7
Adriana Manea <adrianna.manea@gmail.com>
2022-11-08 16:31:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## QR Code Generator
+
+Simple app to generate and download a QR code that can be used to share a link
+
+### [Create a simple JavaScript App to Generate QR Codes](https://www.youtube.com/watch?v=qNiUlml9MDk&list=WL&index=7)
+
+### Resources:
+#### [qrcode.js](https://davidshimjs.github.io/qrcodejs/)
+
+#### [qrcodejs from cdnjs](https://cdnjs.com/libraries/qrcodejs)
+#### [tailwindcss](https://tailwindcss.com/docs/installation)
+  - Play CDN
+  - Customizing your config to insert Poppins font family
---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('generate-form')
+const qr = document.getElementById('qrcode')
+
+const onGenerateSubmit = (e) => {
+  e.preventDefault()
+
+  clearUI()
+  
+  const url = document.getElementById('url').value
+  const size = document.getElementById('size').value
+  // console.log(url, size);
+
+  if(url === '') {
+    alert('Please enter an URL')
+  } else {
+    // if enter the URL -> show spinner
+    showSpinner()
+
+    setTimeout(() => {
+      hideSpinner()
+      generateQRCode(url, size)
+    }, 1000)
+  }
+}
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+  })
+}
+
+const showSpinner = () => {
+  document.getElementById('spinner').style.display = 'block'
+}
+
+const hideSpinner = () => {
+  document.getElementById('spinner').style.display = 'none'
+}
+
+const clearUI = () => {
+  qr.innerHTML = ''
+}
+
+hideSpinner()
+
+form.addEventListener('submit', onGenerateSubmit)
